469fad5bf04cf77c7f9ddd98a767bb7232b81f5f
huhu <huhu@huhudeMacBook-Pro.local>
2022-04-14 17:52:12 +0800

    add variables and examples

--- src/test/java/hellocucumber/Stepdefs.java
+++ src/test/java/hellocucumber/Stepdefs.java
@@ -19,26 +19,18 @@ public class Stepdefs {
     private String today;
     private String actualAnswer;
 
-    @Given("^today is Sunday$")
-    public void today_is_Sunday()  {
-        // Write code here that turns the phrase above into concrete actions
-        today = "Sunday";
-        }
-
-    @Given("^today is Friday")
-    public void today_is_Friday() {
-        this.today = "Friday";
+    @Given("^today is \"([^\"]*)\"$")
+    public void today_is(String today) {
+        this.today = today;
     }
 
     @When("^I ask whether it's Friday yet$")
     public void i_ask_whether_it_s_Friday_yet() {
-        // Write code here that turns the phrase above into concrete actions
         actualAnswer = IsItFriday.isItFriday(today);
     }
 
     @Then("^I should be told \"([^\"]*)\"$")
     public void i_should_be_told(String expectedAnswer)  {
-        // Write code here that turns the phrase above into concrete actions
         assertEquals(expectedAnswer, actualAnswer);
     }
 }
--- src/test/resources/hellocucumber/is_it_friday_yet.feature
+++ src/test/resources/hellocucumber/is_it_friday_yet.feature
@@ -1,12 +1,23 @@
 Feature: Is it Friday yet?
   Everybody wants to know when it's Friday
 
-  Scenario: Sunday isn't Friday
-    Given today is Sunday
-    When I ask whether it's Friday yet
-    Then I should be told "No"
+#  Scenario: Sunday isn't Friday
+#    Given today is Sunday
+#    When I ask whether it's Friday yet
+#    Then I should be told "No"
+#
+#  Scenario: Friday is Friday
+#    Given today is Friday
+#    When I ask whether it's Friday yet
+#    Then I should be told "TGIF"
 
-  Scenario: Friday is Friday
-    Given today is Friday
+  Scenario Outline: Today is or is not Friday
+    Given today is "<day>"
     When I ask whether it's Friday yet
-    Then I should be told "TGIF"
+    Then I should be told "<answer>"
+
+  Examples:
+    | day | answer |
+    | Friday | TGIF |
+    | Sunday | No |
+    | anything else  | No  |
