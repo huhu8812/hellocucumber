da3842f305f2b5ee89fefa6c4a8f4088ad3c7636
huhu <huhu@huhudeMacBook-Pro.local>
2022-04-14 17:06:53 +0800

    add two scenario of today is Friday and today is not Friday

---
+++ src/test/java/hellocucumber/Stepdefs.java
@@ -0,0 +1,44 @@
+package hellocucumber;
+
+import cucumber.api.PendingException;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.When;
+import cucumber.api.java.en.Then;
+import static org.junit.Assert.*;
+
+class IsItFriday {
+    static String isItFriday(String today){
+        if (today.equals("Friday")) {
+            return "TGIF";
+        }
+        return "No";
+    }
+}
+
+public class Stepdefs {
+    private String today;
+    private String actualAnswer;
+
+    @Given("^today is Sunday$")
+    public void today_is_Sunday()  {
+        // Write code here that turns the phrase above into concrete actions
+        today = "Sunday";
+        }
+
+    @Given("^today is Friday")
+    public void today_is_Friday() {
+        this.today = "Friday";
+    }
+
+    @When("^I ask whether it's Friday yet$")
+    public void i_ask_whether_it_s_Friday_yet() {
+        // Write code here that turns the phrase above into concrete actions
+        actualAnswer = IsItFriday.isItFriday(today);
+    }
+
+    @Then("^I should be told \"([^\"]*)\"$")
+    public void i_should_be_told(String expectedAnswer)  {
+        // Write code here that turns the phrase above into concrete actions
+        assertEquals(expectedAnswer, actualAnswer);
+    }
+}
